1327e5c6449581f81dba5d6361d5a420abb8837b
natasha_s <natasl1012@gmail.com>
2023-07-08 12:51:09 +0300

    added tasks 11,12,62 and chng gitignore

---
+++ task11/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая выводит случайное
+// трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+int GetRandomNumber()
+{
+int randNumber = new Random().Next(100, 1000);
+return randNumber;
+}
+
+int GetFirstDigit(int numX)
+{
+    return numX / 100;
+}
+
+int GetLastDigit(int numY)
+{
+    return numY % 10;
+}
+
+void PrintNumber(int numZ)
+{
+    Console.WriteLine(numZ);
+}
+
+void PrintResult(int num1, int num2)
+{
+    Console.WriteLine(num1 + " -> " + num2);
+}
+
+int randNumber = GetRandomNumber();
+PrintNumber(randNumber);
+int firstDigit = GetFirstDigit(randNumber);
+PrintNumber(firstDigit);
+int lastDigit = GetLastDigit(randNumber);
+PrintNumber(lastDigit);
+int resultNumber = (firstDigit * 10 + lastDigit);
+PrintResult(randNumber,resultNumber);
+
+
---
+++ task62/Program.cs
@@ -0,0 +1,54 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру числа.
+
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int GetRandomNumber()
+{
+    int number = new Random().Next(10, 100);
+    return number;
+}
+
+int GetFirstDigit(int number1)
+{
+    // number1 = number 1 / 10 ИЛИ GetRandomNumber / 10;
+    return number1 / 10;
+}
+
+int GetLastDigit(int number1)
+{
+    return number1 % 10;
+}
+
+void PrintNumber(int num1)
+{
+    Console.Write(num1 + " ");
+}
+
+void ShowLargestDigit(int digit1, int digit2)
+{
+    if (digit1 > digit2)
+    {
+        Console.WriteLine("-> " + digit1);
+    }
+    else Console.WriteLine("-> " + digit2);
+}
+
+int num = GetRandomNumber();
+PrintNumber(num);
+int firstDigit = GetFirstDigit(num);
+//PrintNumber(firstDigit);
+int lastDigit = GetLastDigit(num);
+//PrintNumber(lastDigit);
+ShowLargestDigit(firstDigit, lastDigit);
+
+int num2 = GetRandomNumber();
+PrintNumber(num2);
+int firstDigit2 = GetFirstDigit(num2);
+//PrintNumber(firstDigit);
+int lastDigit2 = GetLastDigit(num2);
+//PrintNumber(lastDigit);
+ShowLargestDigit(firstDigit2, lastDigit2);
