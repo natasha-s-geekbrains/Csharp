7913b9452c10f6334771b1c0fbe750b224a0bb15
natasha_s <natasl1012@gmail.com>
2023-07-22 00:10:22 +0300

    added tasks 37,39,40,42

---
+++ task39/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив 
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(min, max);
+    }
+    return res;
+}
+
+void ReverseArray1(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
+
+int[] ReverseArray2(int[] arr)
+{
+    int[] revers = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        revers[i] = arr[arr.Length - 1 - i];
+    }
+    return revers;
+}
+
+int[] array = GetArray(11, 0, 15);
+Console.Write(string.Join(" ", array));
+ReverseArray1(array);
+Console.WriteLine();
+Console.Write(string.Join(" ", array));
+ReverseArray2(array);
+Console.WriteLine();
+Console.Write(string.Join(" ", array));
---
+++ task42/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int num1 = Prompt("Введите десятичное число: ");
+
+string collect = "";
+int result = num1;
+
+while (result > 0)
+{
+    
+    collect = result % 2 + collect;
+
+    result = result / 2;
+}
+
+Console.WriteLine(collect);
+
